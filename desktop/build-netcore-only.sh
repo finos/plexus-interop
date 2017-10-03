5809b203eae1d6e4f8e32c5d1ba89e89a28ea1e1
Alexey Ogarkov <alexey.ogarkov@db.com>
2017-10-03 15:58:11 +0300

    license + mac-build

--- desktop/build-netcore-only.sh
+++ desktop/build-netcore-only.sh
@@ -3,4 +3,4 @@ PATH=/usr/local/bin:/bin:/usr/bin:$PATH:./build/unix/sdk/dotnet/
 
 dotnet restore src/Plexus.Interop.sln /p:CORE_ONLY=true
 dotnet build -c release src/Plexus.Interop.sln /p:CORE_ONLY=true
-dotnet pack -c release -o ./../../../bin/nuget src/Plexus.Interop.sln src/Plexus.Interop.sln /p:CORE_ONLY=true
+dotnet pack -c release -o ./../../../bin/nuget src/Plexus.Interop.sln /p:CORE_ONLY=true
