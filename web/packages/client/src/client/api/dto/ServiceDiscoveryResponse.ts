3784c665ce669a9b6cf016f4f40341e68b6b1d83
Mikhail Udalov <mikhail.udalov@db.com>
2017-10-16 16:01:49 +0300

    Fix lint errors

--- web/packages/client/src/client/api/dto/ServiceDiscoveryResponse.ts
+++ web/packages/client/src/client/api/dto/ServiceDiscoveryResponse.ts
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { clientProtocol as plexus } from "@plexus-interop/protocol";
 import { DiscoveredService } from "./DiscoveredService";
 
 export interface ServiceDiscoveryResponse {
--- web/packages/client/src/client/generic/GenericInvocation.ts
+++ web/packages/client/src/client/generic/GenericInvocation.ts
@@ -219,7 +219,7 @@ export class GenericInvocation {
         switch (this.stateMachine.getCurrent()) {
             case InvocationState.OPEN:
                 this.log.debug("Open state, switching to COMPLETION_RECEIVED");
-                this.readCancellationToken.cancel("Invocation close received")
+                this.readCancellationToken.cancel("Invocation close received");
                 this.stateMachine.go(InvocationState.COMPLETION_RECEIVED);
                 break;
             case InvocationState.COMPLETED:
