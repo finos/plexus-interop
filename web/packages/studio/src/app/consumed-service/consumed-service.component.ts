cf43d87fc697f969e0dc3d9a34d41d6011b3307d
Mikhail Udalov <mikhail.udalov@db.com>
2017-12-18 19:45:55 +0300

    Add invocation code

--- web/packages/studio/src/app/consumed-service/consumed-service.component.ts
+++ web/packages/studio/src/app/consumed-service/consumed-service.component.ts
@@ -5,7 +5,10 @@ import * as fromRoot from '../services/reducers';
 import { Router } from "@angular/router";
 import { SubsctiptionsRegistry } from "../services/SubsctiptionsRegistry";
 import { InteropRegistryService } from "@plexus-interop/broker";
-import { DiscoveredMethod } from "@plexus-interop/client";
+import { DiscoveredMethod, InvocationRequestInfo } from "@plexus-interop/client";
+import { InteropClient } from "../services/InteropClient";
+import { UniqueId } from "@plexus-interop/protocol";
+import { Logger, LoggerFactory } from "@plexus-interop/common";
 
 @Component({
   selector: 'app-consumed-service',
@@ -15,7 +18,11 @@ import { DiscoveredMethod } from "@plexus-interop/client";
 })
 export class ConsumedServiceComponent implements OnInit {
 
+  private readonly log: Logger = LoggerFactory.getLogger("ConsumedServiceComponent");
+
   private registryService: InteropRegistryService;
+
+  private interopClient: InteropClient;
 
   private discoveredMethods: DiscoveredMethod[];
 
@@ -31,22 +38,28 @@ export class ConsumedServiceComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    this.messageContent = "Enter Message Payload in JSON format";
-    
+    this.messageContent = "";
     const consumedMethod$ = this.store
       .filter(state => !!state.plexus.consumedMethod)
       .map(state => state.plexus);
-
     this.subscriptions.add(
       consumedMethod$.subscribe(state => {
+        this.interopClient = state.services.interopClient;
         this.discoveredMethods = state.consumedMethod.discoveredMethods.methods;
       }));
 
   }
 
   sendRequest() {
-    console.log(this.selectedDiscoveredMethod);
+    this.interopClient.sendUnaryRequest(
+      this.toInvocationRequest(this.selectedDiscoveredMethod), this.messageContent, {
+        value: v => {
+          this.log.info(`Response received, ${v}`);
+        },
+        error: e => {
+          this.log.error(`Error received`, e);
+        }
+      });
   }
 
   format(messageStr) {
@@ -56,6 +69,17 @@ export class ConsumedServiceComponent implements OnInit {
   formatAndUpdateArea() {
     this.messageContent = this.format(this.messageContent);
     console.info(this.messageContent);
+  }
+
+  toInvocationRequest(discoveredMethod: DiscoveredMethod): InvocationRequestInfo {
+    const connectionId = discoveredMethod.providedMethod.providedService.connectionId ?
+      UniqueId.fromProperties(discoveredMethod.providedMethod.providedService.connectionId) : null;
+    return {
+      methodId: discoveredMethod.providedMethod.methodId,
+      serviceId: discoveredMethod.providedMethod.providedService.serviceId,
+      applicationId: discoveredMethod.providedMethod.providedService.applicationId,
+      connectionId
+    };
   }
 
 }
