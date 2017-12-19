650fdab29e6e2c472122aaacda5b8ef0e2eacd0e
Mikhail Udalov <mikhail.udalov@db.com>
2017-12-19 14:44:16 +0300

    Enable debug level

--- web/packages/studio/src/app/app.component.ts
+++ web/packages/studio/src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private logger: Logger;
 
   public ngOnInit(): void {
-    LoggerFactory.setLogLevel(LogLevel.INFO);
+    LoggerFactory.setLogLevel(LogLevel.DEBUG);
     this.loggerDelegateRegistration = LoggerFactory.registerDelegate({
 
       log: (logLevel: LogLevel, msg: string, args: any[]) => {
