f31f65c6c25f7500061df368122be67790a27cd7
Anton Nikolaev <anton.nikolaev@db.com>
2018-02-18 13:35:55 +0300

    Simplify build.cmd

--- desktop/build.cmd
+++ desktop/build.cmd
@@ -2,4 +2,4 @@ REM work around for https://github.com/dotnet/cli/issues/3995
 set tmp=
 set temp=
 CD %~dp0
-dotnet restore src\Plexus.Interop.sln && dotnet build -c release src\Plexus.Interop.sln && dotnet pack -c release -o ..\..\..\bin\nuget src\Plexus.Interop.sln
+dotnet pack -c release -o ..\..\..\bin\nuget src\Plexus.Interop.sln /p:Version=0.1.0-CI00052
