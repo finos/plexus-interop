14996f524b98da28e31da2a794319909405b952f
Anton Nikolaev <anton.nikolaev@db.com>
2018-02-18 12:43:41 +0300

    Run dotnet tests on CI

--- desktop/build-netcore-only.sh
+++ desktop/build-netcore-only.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-PATH=/usr/local/bin:/bin:/usr/bin:$PATH:./build/unix/sdk/dotnet/
 
 dotnet restore src/Plexus.Interop.sln /p:CORE_ONLY=true
 dotnet build -c release src/Plexus.Interop.sln /p:CORE_ONLY=true
