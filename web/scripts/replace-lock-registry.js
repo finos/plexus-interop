24f7754c9d4022741afd82441603ca64ccc03ae6
Mikhail Udalov <mikhail.udalov@db.com>
2018-02-20 11:26:21 +0300

    Remove obsolete code

--- web/scripts/replace-lock-registry.js
+++ web/scripts/replace-lock-registry.js
@@ -20,9 +20,8 @@
 const fs = require('fs')
 
 const backward = process.argv.indexOf('--backward') !== -1;
-const paramName = "NPM_LOCK_REGISTRY";
 
-console.log(`Looking for ${paramName} env variable`);
+console.log(`Looking for install registry env variable`);
 
 const value = process.env["NPM_REGISTRY_INSTALL"] || process.env["NPM_REGISTRY"];
 
