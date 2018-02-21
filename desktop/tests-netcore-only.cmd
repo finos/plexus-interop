de65c6d52e2b18d03dcb3b75813b2122c1c58071
Anton Nikolaev <anton.nikolaev@db.com>
2018-02-21 10:20:22 +0300

    Dotnet Test SDK updated to 15.6

--- desktop/tests-netcore-only.cmd
+++ desktop/tests-netcore-only.cmd
@@ -2,4 +2,4 @@ REM work around for https://github.com/dotnet/cli/issues/3995
 set tmp=
 set temp=
 CD %~dp0
-dotnet test src\Plexus.Interop.sln /p:CORE_ONLY=true
+dotnet test src\Plexus.Interop.sln /p:CORE_ONLY=true --test-adapter-path:. --logger:xunit;LogFilePath=loggerFile.xml
--- desktop/tests.cmd
+++ desktop/tests.cmd
@@ -2,4 +2,4 @@ REM work around for https://github.com/dotnet/cli/issues/3995
 set tmp=
 set temp=
 CD %~dp0
-dotnet test src\Plexus.Interop.sln
+dotnet test src\Plexus.Interop.sln --test-adapter-path:. --logger:xunit;LogFilePath=loggerFile.xml
