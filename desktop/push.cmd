c303759cfed1f186a1aa358723982fd10de5ad8e
Anton Nikolaev <anton.nikolaev@db.com>
2018-03-16 19:21:27 +0300

    Increased nuget push timeout

--- desktop/push.cmd
+++ desktop/push.cmd
@@ -2,4 +2,7 @@ REM work around for https://github.com/dotnet/cli/issues/3995
 set tmp=
 set temp=
 CD %~dp0
-dotnet nuget push ..\bin\nuget\*.nupkg -s %PLEXUS_BUILD_NUGET_SOURCE% -k %PLEXUS_BUILD_NUGET_API_KEY%
+if not defined PLEXUS_BUILD_NUGET_PUSH_TIMEOUT (
+	SET PLEXUS_BUILD_NUGET_PUSH_TIMEOUT=3600
+)
+dotnet nuget push ..\bin\nuget\*.nupkg -s %PLEXUS_BUILD_NUGET_SOURCE% -k %PLEXUS_BUILD_NUGET_API_KEY% -t %PLEXUS_BUILD_NUGET_PUSH_TIMEOUT%
