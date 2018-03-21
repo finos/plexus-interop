3ee51ad0acf5a595ae2b64a9aec424813185683e
Mikhail Udalov <mikhail.udalov@db.com>
2018-03-21 19:39:21 +0300

    Add option

--- web/packages/cli/src/commands/GenTsCommand.ts
+++ web/packages/cli/src/commands/GenTsCommand.ts
@@ -4,6 +4,11 @@ import * as commander from 'commander';
 export class GenTsCommand implements Command {
     
     public register(builder: commander.CommanderStatic): void {
-        builder.command('gen-ts');
+        builder.command('gen-ts')
+            .option('--baseDir <baseDir>', 'Plexus Metadata base directory')
+            .action((opts) => {
+                // tslint:disable-next-line:no-console
+                console.log(opts.baseDir);
+            });
     }
 }
