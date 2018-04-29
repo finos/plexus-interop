1f0f9e33abff389f4af3976b1bf0ef5f7c73e0f2
Mikhail Udalov <mikhail.udalov@db.com>
2018-04-29 09:26:17 +0300

    Add global refs to lang configs

---
+++ dsl/interop-lang/src/main/java/com/db/plexus/interop/dsl/GlobalLangConfigHolder.java
@@ -0,0 +1,11 @@
+package com.db.plexus.interop.dsl;
+
+import com.db.plexus.interop.dsl.protobuf.ProtoLangConfig;
+
+public class GlobalLangConfigHolder {
+
+    public static ProtoLangConfig protoLangConfig;
+
+    public static ProtoLangConfig interopLangConfig;
+
+}
