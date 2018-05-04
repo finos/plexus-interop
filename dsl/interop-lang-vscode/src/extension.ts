72bb120ba1c94efa9d3f8e5c41f4c0467a35e8eb
Mikhail Udalov <mikhail.udalov@db.com>
2018-05-04 11:53:45 +0300

    Code clean up

--- dsl/interop-lang-vscode/src/extension.ts
+++ dsl/interop-lang-vscode/src/extension.ts
@@ -1,5 +1,19 @@
-
-
+/**
+ * Copyright 2017-2018 Plexus Interop Deutsche Bank AG
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as net from 'net';
 import * as path from 'path';
 
@@ -21,7 +35,6 @@ export function activate(context: ExtensionContext) {
             interopClientOptions);
     
     context.subscriptions.push(interopClient.start());
-
 
     const protoClientOptions: LanguageClientOptions = {
         documentSelector: [
@@ -67,12 +80,4 @@ function createLangClient(
     }   
     
     return new LanguageClient(title, serverOptions, clientOptions);
-}
-
-function isDebugMode(): boolean {
-	let args = (process as any).execArgv;
-	if (args) {
-		return args.some((arg) => /^--debug=?/.test(arg) || /^--debug-brk=?/.test(arg) || /^--inspect-brk=?/.test(arg));
-	}
-	return false;
 }
