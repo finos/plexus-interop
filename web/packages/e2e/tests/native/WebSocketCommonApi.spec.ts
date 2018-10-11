b59399931f66dc57a5d79ce9a4ad226e5211ec34
Mikhail Udalov <mikhail.udalov@db.com>
2018-10-11 21:16:42 +0300

    Add invocation test

--- web/packages/e2e/tests/native/WebSocketCommonApi.spec.ts
+++ web/packages/e2e/tests/native/WebSocketCommonApi.spec.ts
@@ -15,30 +15,63 @@
  * limitations under the License.
  */
 import { readWsUrl } from '../common/utils';
-import { InteropPlatformFactory, InteropPlatform } from '@plexus-interop/common-api-impl';
+import { InteropPlatformFactory, InteropPlatform, MethodImplementation, InteropPeerDescriptor } from '@plexus-interop/common-api-impl';
 import { expect } from 'chai';
+import { BaseEchoTest } from '../echo/BaseEchoTest';
+import { ClientsSetup } from '../common/ClientsSetup';
 
 // tslint:disable:no-unused-expression
 describe('Client: Common API Implementation', () => {
 
-    const webSocketUrl = readWsUrl();    
+    const webSocketUrl = readWsUrl();
 
     const factory = new InteropPlatformFactory();
 
+    const clientsSetup = new ClientsSetup();
+    const testUtils = new BaseEchoTest();
+
     it('Creates Interop Platform Factory', async () => {
-        
-        const platform = await factory.createPlatform({ webSocketUrl });    
+
+        const platform = await factory.createPlatform({ webSocketUrl });
         expect(platform).to.not.be.undefined;
-              
+
     });
 
     it('Connects to Broker', async () => {
-        
-        const platform: InteropPlatform = await factory.createPlatform({ webSocketUrl });    
+
+        const platform: InteropPlatform = await factory.createPlatform({ webSocketUrl });
         const client = await platform.connect('echo-client');
         expect(client).to.not.be.undefined;
-        await client.disconnect();   
-         
+        await client.disconnect();
+
+    });
+
+    it('Sends request and receives response', async () => {
+
+        const platform: InteropPlatform = await factory.createPlatform({ webSocketUrl });
+        let invoked = false;
+        const method: MethodImplementation = {
+            name: 'unary-method',
+            onInvoke: async (args: any, caller: InteropPeerDescriptor) => {
+                invoked = true;
+                return args;
+            }
+        };
+
+        const client = await platform.connect('echo-client');
+        const server = await platform.connect('echo-server', undefined, [method]);
+        const request = clientsSetup.createRequestDto();
+        const response = await client.invoke('unary-method', request);
+        
+        expect(invoked).to.be.true;        
+        expect(response).to.not.be.undefined;
+        testUtils.assertEqual(request, response);
+        expect(client).to.not.be.undefined;
+        expect(server).to.not.be.undefined;
+
+        await client.disconnect();
+        await server.disconnect();
+
     });
 
 });
