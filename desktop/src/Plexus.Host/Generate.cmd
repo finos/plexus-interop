b34b03b783e03eff096cc3969267be1dccc29695
Anton Nikolaev <anton.nikolaev@db.com>
2019-08-01 09:12:15 +0300

    Fixed paths to protoc

--- desktop/src/Plexus.Host/Generate.cmd
+++ desktop/src/Plexus.Host/Generate.cmd
@@ -3,7 +3,7 @@ set INTEROP_METADATA_PATH=..\..\..\dsl\interop-lang\src\main\resources
 if not defined NUGET_PACKAGES (
   set NUGET_PACKAGES=%USERPROFILE%\.nuget\packages
 )
-set PROTOC_PATH=%NUGET_PACKAGES%\google.protobuf.tools\3.5.1\tools\windows_x86\protoc.exe
+set PROTOC_PATH=%NUGET_PACKAGES%\google.protobuf.tools\3.6.1\tools\windows_x86\protoc.exe
 
 set INTEROP_MANIFEST_PATH=command_line_tool.interop
 set CSHARP_NAMESPACE=internal_access:Plexus.Host.Internal.Generated
