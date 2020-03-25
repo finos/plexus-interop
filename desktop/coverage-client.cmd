23a54c7e1ec3308f173150b5acbbeee4467054dc
Maksim Mukhamatulin <maksim.mukhamatulin@db.com>
2020-03-25 18:24:32 +0300

    Fixed paths to generate coverage reports

--- desktop/coverage-client.cmd
+++ desktop/coverage-client.cmd
@@ -15,7 +15,7 @@ if not defined NUGET_PACKAGES (
 if not exist ..\bin\test-reports mkdir ..\bin\test-reports
 
 CALL dotnet restore src\Plexus.Interop.sln && dotnet build src\Plexus.Interop.sln -c debug /p:DebugType=Full && SET PLEXUS_TIMEOUT_MULTIPLIER=10 ^
-  && %NUGET_PACKAGES%\OpenCover\4.6.519\tools\OpenCover.Console.exe -oldStyle -returntargetcode -register:user -output:..\bin\test-reports\dotnet-coverage.xml ^
+  && %NUGET_PACKAGES%\OpenCover\4.7.922\tools\OpenCover.Console.exe -oldStyle -returntargetcode -register:user -output:..\bin\test-reports\dotnet-coverage.xml ^
   -filter:"+[Plexus.*]* -[*.Tests]* -[*.IntegrationTests]* -[*.Testing]* -[Plexus.Interop.Samples.*]* -[Plexus.*]*.Generated.* -[Plexus.Interop.Broker*]* -[Plexus.Interop.Apps.Manager*]* -[Plexus.Interop.Metamodel*]* -[*.Server]*" -excludebyfile:*.proto.cs;*.g.cs -skipautoprops ^
   -targetdir:src -target:dotnet.exe -searchdirs:..\bin\win-x86\broker -targetargs:"test Plexus.Interop.Tests.sln /p:DebugType=Full --test-adapter-path:. --logger:%LOGGER% --verbosity quiet" ^
-  && %NUGET_PACKAGES%\ReportGenerator\3.1.2\tools\ReportGenerator.exe -reports:..\bin\test-reports\dotnet-coverage.xml -targetdir:..\bin\test-reports\dotnet-coverage -sourcedirs:src reporttypes:Html
+  && %NUGET_PACKAGES%\ReportGenerator\4.0.14\tools\net47\ReportGenerator.exe -reports:..\bin\test-reports\dotnet-coverage.xml -targetdir:..\bin\test-reports\dotnet-coverage -sourcedirs:src reporttypes:Html
