c341167e069c1a4b917336aad65d989a89d716be
Iskender Kamchibekov <iskender.kamchibekov@db.com>
2021-10-29 19:59:51 +0300

    AAP-21397: Add WebSocketSecureTransportTests

---
+++ desktop/src/Plexus.Interop.Transport.WebSockets.Tests/WebSocketSecureTransportTests.cs
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2017-2021 Plexus Interop Deutsche Bank AG
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace Plexus.Interop.Transport.WebSockets
+{
+    using Plexus.Interop.Transport.Protocol.Protobuf;
+    using Plexus.Interop.Transport.Transmission.WebSockets.Client;
+    using Plexus.Interop.Transport.Transmission.WebSockets.Server;
+    using System.Security.Cryptography.X509Certificates;
+    using Xunit.Abstractions;
+
+    public sealed class WebSocketSecureTransportTests : TransportTestsSuite
+    {
+        public WebSocketSecureTransportTests(ITestOutputHelper output) : base(output)
+        {
+            var cert = new X509Certificate2("mycert.pfx", "god");
+            Server = RegisterDisposable(TransportServerFactory.Instance.Create(
+                WebSocketTransmissionServerFactory.Instance.CreateSecure(new WebSocketTransmissionServerOptions(BrokerWorkingDir), cert),
+                new ProtobufTransportProtocolSerializationProvider()));
+            Client = TransportClientFactory.Instance.Create(
+                new WebSocketTransmissionClient(),
+                new ProtobufTransportProtocolSerializationProvider());
+        }
+
+        protected override ITransportServer Server { get; }
+
+        protected override ITransportClient Client { get; }
+    }
+}
