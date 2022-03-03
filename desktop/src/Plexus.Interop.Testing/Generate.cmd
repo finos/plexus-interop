692209f6dfa3f4ede99cacd93f0d30887725c25f
Iskender Kamchibekov <iskender.kamchibekov@db.com>
2022-03-03 19:51:52 +0300

    AAP-20243: move testing metadata

--- desktop/src/Plexus.Interop.Testing/Generate.cmd
+++ desktop/src/Plexus.Interop.Testing/Generate.cmd
@@ -1,2 +1,2 @@
-CALL plexus gen-json-meta -b TestBrokerConfig/metadata/interop -o TestBrokerConfig/metadata --verbose
-CALL plexus gen-csharp -b TestBrokerConfig/metadata/interop -o Generated -i {echo_client.interop,echo_server.interop,test_app_launcher.interop} -n Plexus.Interop.Testing.Generated --verbose
+call plexus gen-json-meta -b ..\..\..\dsl\interop-lang\src\main\resources -o TestBrokerConfig\metadata --verbose
+call plexus gen-csharp -b ..\..\..\dsl\interop-lang\src\main\resources -o Generated -i {echo_client.interop,echo_server.interop,test_app_launcher.interop} -n Plexus.Interop.Testing.Generated --verbose
