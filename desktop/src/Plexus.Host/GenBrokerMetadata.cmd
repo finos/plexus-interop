3703fa3b0595b61a379f84663d46a35fde9c56f7
Iskender Kamchibekov <iskender.kamchibekov@db.com>
2022-03-28 12:20:09 +0300

    AAP-21858: Clean up Plexus.Host

--- desktop/src/Plexus.Host/GenBrokerMetadata.cmd
+++
@@ -1,2 +0,0 @@
-CD %~dp0
-java -jar %~dp0../../sdk/plexusgen.jar --type=json_meta --baseDir=registry --out=metadata
--- desktop/src/Plexus.Host/LaunchBroker.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar ../sdk/plexusgen.jar --type=json_meta --baseDir=metadata/interop --out=interop && ./../broker/plexus broker
